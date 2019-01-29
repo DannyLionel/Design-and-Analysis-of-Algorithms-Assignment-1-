2edf5d4b9cbce9d37adc6bd1279827a1a980ccf8
Dhanushga Lionel <32670499+DhanushgaLionel@users.noreply.github.com>
2019-01-29 14:26:02 -0500

    Update Assignment1.cpp

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -115,29 +115,21 @@ continue;
 MyQueue2.push_back({ x2, y2 });
 distance[y2][x2];
 }
-/*
 double maxAngle = tan(v*v / (G*d));//max angle to
-
-get most height and distance
-
+//get most height and distance
 double factor = h / d;
 if (tan(maxAngle) - G * d / (2 * v*v*cos(maxAngle)*
-
 cos(maxAngle)) < factor)// max jump doesn't get there
 {
 return false;
 }
 double low = maxAngle, high = PI / 2; //Binary
-
 search tree for finding jumping angle
-
 for (int i = 0; i < 100; i++)
 {
 double mid = (low + high) / 2;
 double rhs = tan(mid) - G * d / (2 *
-
 v*v*cos(mid)*cos(mid));//Change RHS Variable Name
-
 if (rhs > factor)//adjust low or high
 {
 low = mid;
@@ -147,35 +139,16 @@ else
 high = mid;
 }
 }
-*/
 bool result = false;
-
-for (int x3 = min(x, x2); !result && x3 <= max(x, x2);
-
-x3++)
-
+for (int x3 = min(x, x2); !result && x3 <= max(x, x2);x3++)
 {
 for (int y3 = min(y, y2); !result && y3 <=
-
 max(y, y2); y3++)
-
 {
-result |= Check(vd / w, VH, x2 - x, y2
-
-- y, x3 + 0.5 - x, y3 - 0.5 - y, y3 + 0.5 - y, grid[y3][x3] - grid[y][x]);
-
-result |= Check(vd / w, VH, x2 - x, y2
-
-- y, x3 - 0.5 - x, y3 - 0.5 - y, y3 + 0.5 - y, grid[y3][x3] - grid[y][x]);
-
-result |= Check(vd / w, VH, y2 - y, x2
-
-- x, y3 + 0.5 - y, x3 - 0.5 - x, x3 + 0.5 - x, grid[y3][x3] - grid[y][x]);
-
-result |= Check(vd / w, VH, y2 - y, x2
-
-- x, y3 - 0.5 - y, x3 - 0.5 - x, x3 + 0.5 - x, grid[y3][x3] - grid[y][x]);
-
+result |= Check(vd / w, VH, x2 - x, y2- y, x3 + 0.5 - x, y3 - 0.5 - y, y3 + 0.5 - y, grid[y3][x3] - grid[y][x]);
+result |= Check(vd / w, VH, x2 - x, y2- y, x3 - 0.5 - x, y3 - 0.5 - y, y3 + 0.5 - y, grid[y3][x3] - grid[y][x]);
+result |= Check(vd / w, VH, y2 - y, x2- x, y3 + 0.5 - y, x3 - 0.5 - x, x3 + 0.5 - x, grid[y3][x3] - grid[y][x]);
+result |= Check(vd / w, VH, y2 - y, x2- x, y3 - 0.5 - y, x3 - 0.5 - x, x3 + 0.5 - x, grid[y3][x3] - grid[y][x]);
 }
 if (result)
 {
@@ -184,11 +157,8 @@ continue;
 MyQueue2.push_back({ x2, y2 });
 distance[y2][x2];
 }
-/*
 double Theta = low; //Assigning jumping
-
 angle//change variable name
-
 int dx = x2 - lx;
 int dy = y2 - ly;
 if (dx != 0)
@@ -196,51 +166,27 @@ if (dx != 0)
 int StepX = dx / abs(dx);
 int x3 = lx + StepX;
 double y3 = ly + 0.5 + (y2 - ly) /
-
 (2.0*abs(dx));
-
 for (int i = 0; i < abs(dx); i++)
 {
 int ThisY = int(y3 + 1e-9);
 int MustReach = grid[x3][ThisY];
 MustReach = max(MustReach,
-
 grid[x3 - StepX][ThisY]);
-
 //Borderline case to reach height
 if (y3 - ThisY < 1e-8)
 {
-
-MustReach =
-
-max(MustReach, grid[x3][ThisY - 1]);
-
-MustReach =
-
-max(MustReach, grid[x3 - StepX][ThisY - 1]);
-
+MustReach =max(MustReach, grid[x3][ThisY - 1]);
+MustReach =max(MustReach, grid[x3 - StepX][ThisY - 1]);
 }
 //Calculate horizontal and vertical
-
-Movemen
-
-double HMove = w * sqrt(i + 0.5)*(i +
-
-0.5) + (y3 - ly - 0.5)*(y3 - ly - 0.5);
-
-double T = HMove /
-
-(v*cos(Theta));//Think this is Time value Change variable to TIme
-
+//Movemen
+double HMove = w * sqrt(i + 0.5)*(i +0.5) + (y3 - ly - 0.5)*(y3 - ly - 0.5);
+double T = HMove /(v*cos(Theta));//Think this is Time value Change variable to TIme
 double MyH = v * sin(Theta)* T -
-
 0.5*G*T*T;
-
 //If vertical not made
-if (MyH < MustReach, grid[lx][ly] +
-
-1e-10);
-
+if (MyH < MustReach, grid[lx][ly] +1e-10);
 {
 return false;
 }
@@ -248,9 +194,7 @@ x3 += StepX;
 y3 += (y2 - ly) / (1.0 *abs(dy));
 }
 }
-*/
 }
-
 }
 MyQueue.swap(MyQueue2);
 }
@@ -268,7 +212,6 @@ if (distance[y][x] > dx*dy)
 cout << "X";
 }
 else
-
 {
 cout << distance[y][x];
 }
@@ -277,65 +220,33 @@ cout << endl;
 }
 }
 }
-
-/*
 if (dy != 0)
 {
 int StepY = dy / abs(dy);
 int y3 = ly + StepY;
-double x3 = lx + 0.5 + (x2 - lx) /
-
-(2.0*abs(dy));
-
+double x3 = lx + 0.5 + (x2 - lx) /(2.0*abs(dy));
 for (int i = 0; i < abs(dy); i++)
 {
 int ThisX = int(x3 + 1e-9);
-int MustReach = grid[ThisX][y3];//We
-
-must reach building this high
-
+int MustReach = grid[ThisX][y3];
+//We must reach building this high
 MustReach = max(MustReach,
-
 grid[ThisX][y3 - StepY]);
-
 if (x - ThisX < 1e-8)//Another
-
 Borderline case
-
 {
-MustReach =
-
-max(MustReach, grid[ThisX - 1][y3]);
-
-MustReach =
-
-max(MustReach, grid[ThisX - 1][y3 - StepY]);
-
+MustReach =max(MustReach, grid[ThisX - 1][y3]);
+MustReach =max(MustReach, grid[ThisX - 1][y3 - StepY]);
 }
-//Calculating our horizontal and
-
-vertical positions
-
-double HMove = w * sqrt((i + 0.5)*(i
-
-+ 0.5) + (x3 - lx - 0.5)*(x3 - lx - 0.5));
-
-double T = HMove /
-
-(v*cos(Theta));//change T variable to Time Variable
-
-double MyH = v * sin(Theta) * T - 0.5
-
-* G*T*T;
-
+//Calculating our horizontal and vertical positions
+double HMove = w * sqrt((i + 0.5)*(i+ 0.5) + (x3 - lx - 0.5)*(x3 - lx - 0.5));
+double T = HMove /(v*cos(Theta));
+//change T variable to Time Variable
+double MyH = v * sin(Theta) * T - 0.5* G*T*T;
 //Did not make it high enough
-if (MyH < MustReach, grid[lx][ly] =
-
-1e-10)
-
+if (MyH < MustReach, grid[lx][ly] =1e-10)
 {
 return false;
-
 }
 x3 += (x2 - lx) / (1.0*abs(dy));
 y3 += StepY;
@@ -368,35 +279,17 @@ SDL_bool done = SDL_FALSE;
 while (!done) {
 SDL_Event event;
 SDL_SetRenderDrawColor(renderer, 0, 0, 0,
-
 SDL_ALPHA_OPAQUE);
-
 SDL_RenderClear(renderer);
 SDL_SetRenderDrawColor(renderer, 255, 255, 255,
-
 SDL_ALPHA_OPAQUE);
-
 for (int y = 1; y <= dyi; y++) {
 for (int x = 1; x <= dxi; x++) {
-SDL_RenderDrawLine(renderer, (x - 1)*w, (y -
-
-1)*w, x*w, (y-1)*w);
-
-SDL_RenderDrawLine(renderer, (x - 1)*w, (y -
-
-1)*w, (x - 1)*w, y*w);
-
-SDL_RenderDrawLine(renderer, (x - 1)*w, (y)*w,
-
-(x)*w, y*w);
-
-SDL_RenderDrawLine(renderer, (x)*w, (y - 1)*w,
-
-(x)*w, y*w);
-
-}
-}
-
+SDL_RenderDrawLine(renderer, (x - 1)*w, (y -1)*w, x*w, (y-1)*w);
+SDL_RenderDrawLine(renderer, (x - 1)*w, (y -1)*w, (x - 1)*w, y*w);
+SDL_RenderDrawLine(renderer, (x - 1)*w, (y)*w,(x)*w, y*w);
+SDL_RenderDrawLine(renderer, (x)*w, (y - 1)*w,(x)*w, y*w);
+}}
 SDL_RenderPresent(renderer);
 while (SDL_PollEvent(&event)) {
 if (event.type == SDL_QUIT) {
